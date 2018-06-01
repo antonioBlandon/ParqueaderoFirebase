5867caadae8ca2c03d8292c8c912ea3243b079ef
Jose Blandon <jose.blandon@ceiba.com.co>
2018-06-01 13:41:02 -0500

    test en registroPresenter

--- app/src/test/java/co/com/ceiba/parqueaderofirebase/registrar/RegistroPresenterTest.java
+++ app/src/test/java/co/com/ceiba/parqueaderofirebase/registrar/RegistroPresenterTest.java
@@ -6,8 +6,20 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import co.com.ceiba.parqueaderofirebase.data.entities.Parqueadero;
+import co.com.ceiba.parqueaderofirebase.data.entities.Vehiculo;
+
+import static co.com.ceiba.parqueaderofirebase.ParqueaderoBuilder.aParking;
+import static co.com.ceiba.parqueaderofirebase.VehiculoBuilder.aVehicle;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -48,7 +60,17 @@ public class RegistroPresenterTest {
     }
 
     @Test
+    public void validarCamposNulosPlacaVacia() {
+        String placa = "";
+        presenter.validarCamposNulos(placa,anyString());
+        verify(view).showError(anyInt());
+    }
+
+    @Test
     public void validarCamposNulos() {
+        String placa = "";
+        presenter.validarCamposNulos(placa,anyString());
+        presenter.validarPlaca(aVehicle().build());
     }
 
     @Test
@@ -65,5 +87,11 @@ public class RegistroPresenterTest {
 
     @Test
     public void validarPlacaExiste() {
+        Vehiculo vehiculo = aVehicle().build();
+        List<Vehiculo> list = new ArrayList<>();
+        list.add(vehiculo);
+        presenter.validarPlacaExiste(list,vehiculo);
+        verify(view).showError(anyInt());
     }
+
 }
